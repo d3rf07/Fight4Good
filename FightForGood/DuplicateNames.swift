7bacd98a94b6bf6df037db8cf02ab0dfc64b910e
d3rf <d3rf@d3rf.com>
2019-02-13 23:51:12 +0100

    do

---
+++ FightForGood/DuplicateNames.swift
@@ -0,0 +1,19 @@
+//
+//  Team.swift
+//  FightForGood
+//
+//  Created by d3rf on 09/08/2018.
+//  Copyright © 2018 d3rf. All rights reserved.
+//
+
+import Foundation
+
+struct duplicateNames {
+    
+    //memory arrays with team names to avoid duplication
+    static var memTeamNames: [String] = []
+    
+    //memory arrays with Characters names to avoid duplication
+    static var memCharacterNames: [String] = []
+    
+}
